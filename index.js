b889306f99b80cd270a2db8d3fa8d650d386a340
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-09-29 22:16:38 +0200

    updated index.js for endpoints routing

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-//importing express and morgan and assignning to a variable
+//importing the modules and assignning to a variable
 const express = require("express"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
+
+app.use(bodyParser.json());
 
 //the movie data
 let movies = [
@@ -58,6 +62,18 @@ let movies = [
   }
 ];
 
+//the user data
+let users = [
+  {
+    id: "txsddf0222-xr2t-0000",
+    name: "Brad Tomson",
+    birthdate: "01.10.1990",
+    username: "bratom1",
+    email: "tomsonb@mail.com",
+    password: "son1990"
+  }
+];
+
 //Using the Morgan middleware library to log all requests
 app.use(morgan("common"));
 
@@ -66,9 +82,77 @@ app.get("/", (req, res) => {
   res.send("Welcome to CinemaFlix!");
 });
 
-//an express route returning movie data
+//Returns a data about list of all movies
 app.get("/movies", (req, res) => {
   res.json(movies);
+});
+
+//Returns a data about a single movie by title
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find(movie => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+
+//Returns a list of all movie genres
+app.get("/genres", (req, res) => {
+  res.send("Successful GET request returning data on all movie genres");
+});
+
+//Returns a list of all movie directors
+app.get("/directors", (req, res) => {
+  res.send("Successful GET request returning data on all movie directors");
+});
+
+//Returns the data of a single movie director
+app.get("/directors/:name", (req, res) => {
+  res.send(
+    "Successful GET request returning data on a single movie director by name"
+  );
+});
+
+//Returns the list of all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+//Adds a new user to the list of users
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = "Missing name in request body";
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//Update the user info  by name
+app.put("/users/:name/:username", (req, res) => {
+  let user = users.find(user => {
+    return user.name === req.params.name;
+  });
+
+  if (user) {
+    user.username = req.params.username;
+    res
+      .status(201)
+      .send(
+        "User " +
+          req.params.name +
+          " has changed the username into " +
+          req.params.username
+      );
+  } else {
+    res
+      .status(404)
+      .send("User with the name " + req.params.name + " was not found.");
+  }
 });
 
 //exposing files in 'public' folder
