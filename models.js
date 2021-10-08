06b864937d57a6af1389403e573f23b57d1c0663
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-08 14:31:47 +0200

    created and exported the movie & user models

--- models.js
+++ models.js
@@ -1,1 +1,39 @@
-const mongoose = require ('mongoose');
+//importing mongoose
+const mongoose = require("mongoose");
+
+//creating the movie schema
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  ReleaseYear: Number,
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    BirthYear: Number
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
+});
+
+//creating the user schema
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
+//creating the movie model
+let Movie = mongoose.model("Movie", movieSchema);
+//creating the user model
+let User = mongoose.model("User", userSchema);
+
+//exporting the models
+module.exports.Movie = Movie;
+module.exports.User = User;
