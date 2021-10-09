e270a0ef28bbd5dc2b000d09fdaeb93a72d05b16
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-09 17:51:55 +0200

    added and exported genre and director models

--- models.js
+++ models.js
@@ -29,11 +29,30 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
 
+//creating the genre schema
+let genreSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Description: { type: String, required: true }
+});
+
+//creating the director schema
+let directorSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Bio: { type: String, required: true },
+  BirthYear: Number
+});
+
 //creating the movie model
 let Movie = mongoose.model("Movie", movieSchema);
 //creating the user model
 let User = mongoose.model("User", userSchema);
+//creating the movie model
+let Genre = mongoose.model("Genre", genreSchema);
+//creating the user model
+let Director = mongoose.model("Director", directorSchema);
 
 //exporting the models
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
