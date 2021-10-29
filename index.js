3862a6af8bd8666e73af043ed0f3b11826489d47
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2021-10-29 17:53:22 +0200

    added authentication logic to movies endpoint

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
 
 //Returns a data about list of all movies
 app.get("/movies",
-  //passport.authenticate("jwt", { session: false }),
+  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.find()
       .then(movies => {
